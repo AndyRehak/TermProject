5566224f608f165d9ac97233310199b281f2b8f7
AndyRehak <38256397+AndyRehak@users.noreply.github.com>
2018-04-10 11:06:32 -0500

    Update Test.m

--- Test.m
+++ Test.m
@@ -2,4 +2,18 @@ clear;
 clc;
 close all;
 fclose all;
-fileID=fopen('01.xlsx')
+
+Strain1=xlsread('01.xlsx','sheet1','A:A');
+Mat1=xlsread('01.xlsx','sheet1','B:B');
+
+Strain2=xlsread('01.xlsx','sheet2','A:A');
+Mat2=xlsread('01.xlsx','sheet2','B:B');
+
+Strain3=xlsread('01.xlsx','sheet3','A:A');
+Mat3=xlsread('01.xlsx','sheet3','B:B');
+
+Strain4=xlsread('01.xlsx','sheet4','A:A');
+Mat4=xlsread('01.xlsx','sheet4','B:B');
+
+Strain5=xlsread('01.xlsx','sheet5','A:A');
+Mat5=xlsread('01.xlsx','sheet5','B:B');
