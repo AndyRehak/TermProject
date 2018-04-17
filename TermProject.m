7d9c4cd1896c1699f8dd426afd3a715d2344c18d
AndyRehak <38256397+AndyRehak@users.noreply.github.com>
2018-04-17 11:23:17 -0500

    Update TermProject.m
    This is just for strain 1 for now.

--- TermProject.m
+++ TermProject.m
@@ -41,6 +41,36 @@ toughness5=trapz(Mat5)
 
 
 %C
+Value1=.2;
+Value2=.5;
+Value3=.7;
+n1=length(Strain1);
+D1=inf;
+D2=inf;
+D3=inf;
+for i=1:n1
+    temp1=abs(Strain1(i)-Value1);
+    if temp1<D1
+        D1=temp1;
+        x1=i;
+    end
+    temp2=abs(Strain1(i)-Value2);
+    if temp2<D2
+        D2=temp2;
+        x2=i;
+    end
+    temp3=abs(Strain1(i)-Value3);
+    if temp3<D3
+        D3=temp3;
+        x3=i;
+    end
+end
+h1=(Strain1(x1+2)-Strain1(x1-2))/5;
+h2=(Strain1(x2+2)-Strain1(x2-2))/5;
+h3=(Strain1(x3+2)-Strain1(x3-2))/5;
+Young1_Value1=(-Mat1(x1+2)+8*Mat1(x1+1)-8*Mat1(x1-1)+Mat1(x1-2))/(12*h1);
+Young1_Value2=(-Mat1(x2+2)+8*Mat1(x2+1)-8*Mat1(x2-1)+Mat1(x2-2))/(12*h2);
+Young1_Value3=(-Mat1(x3+2)+8*Mat1(x3+1)-8*Mat1(x3-1)+Mat1(x3-2))/(12*h3);
 
 
 %D
